86e4f1fb6c67df6a3eab047e5440fd17ab103724
James McMath <mcmathja@gmail.com>
2015-12-02 17:53:07 -0500

    Add test harness

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import Stream from './src/Stream'
 import Property from './src/Property'
+import * as Constants from './src/Constants'
 import * as Constructors from './src/Constructors'
 
 /**
@@ -12,6 +13,6 @@ var Narly = {
   Property: Property
 }
 
-Object.assign(Narly, Constructors)
+Object.assign(Narly, Constants, Constructors)
 
 module.exports = Narly
